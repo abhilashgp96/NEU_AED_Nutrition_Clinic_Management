34b63afbcc5811e19b1ae0467d8172b29e4796e3
Abhilash G P <gowdrupalakshappa.a@northeastern.edu>
2022-11-28 19:46:08 -0500

    First Commit

---
+++ Hotel_Management_Final_AED/src/main/java/com/alphas/mycompany/hotel_management_final_aed/Hotel_Management_Final_AED.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.alphas.mycompany.hotel_management_final_aed;
+
+/**
+ *
+ * @author User
+ */
+public class Hotel_Management_Final_AED {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
