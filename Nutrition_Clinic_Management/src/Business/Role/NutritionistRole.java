ef4268bbb6ab33f148eb1b18bbc63471404dee58
AnvitaPatil <patil.anv@northeastern.edu>
2022-12-08 13:48:44 -0500

    added all roles

---
+++ Nutrition_Clinic_Management/src/Business/Role/NutritionistRole.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.Enterprise.Enterprise;
+import Business.Environment;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Anvita
+ */
+public class NutritionistRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Enterprise enterprise,Environment business){
+    
+        return null;
+    }
+    
+}
