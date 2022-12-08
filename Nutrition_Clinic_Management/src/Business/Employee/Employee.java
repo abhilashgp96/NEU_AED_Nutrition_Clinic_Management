8968c7de5129d97dce11962ec578d6755ec81a2c
Abhilash G P <gowdrupalakshappa.a@northeastern.edu>
2022-12-08 00:11:58 -0500

    Added the Employee Pojo

---
+++ Nutrition_Clinic_Management/src/Business/Employee/Employee.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Employee;
+
+/**
+ *
+ * @author raunak
+ */
+public class Employee {
+    
+    private String empName;
+    private int empId;
+    private static int count = 1;
+
+    public Employee() {
+        empId = count;
+        count++;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    
+    public String getEmpName() {
+        return empName;
+    }
+
+    @Override
+    public String toString() {
+        return empName;
+    }
+    
+    
+}
