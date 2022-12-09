a0d1f40d7fcfee590ea79fbee25e87560b9aa6b7
rayappanor.s@northeastern.edu <rayappanor.s@northeastern.edu>
2022-12-09 03:12:20 -0500

    Updating the Organization Class

--- Nutrition_Clinic_Management/src/Business/Organization/Organization.java
+++ Nutrition_Clinic_Management/src/Business/Organization/Organization.java
@@ -4,10 +4,41 @@
  */
 package Business.Organization;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author soumya
  */
-public class Organization {
+public abstract class Organization {
+    
+    private String orgName;
+    private WorkQueue workQueue;
+    private EmployeeDirectory empDirectory;
+    private UserAccountDirectory userAccountDirectory;
+    private int orgID;
+    private static int counter=0;
+    
+    public enum Type{
+        Admin("Admin Organization"), Nutritionist("Nutritionist Organization"), Lab("FDA Lab Organization"), Caterer("Caterer Organization"), DishFood("Food Supplier Organization"), Ingredient("Ingredient supplier Organization");
+        private String value;
+        private Type(String value) {
+            this.value = value;
+        }
+        public String getValue() {
+            return value;
+        }
+    }
+    
+    public Organization(String name) {
+        this.orgName = name;
+        workQueue = new WorkQueue();
+        empDirectory = new EmployeeDirectory();
+        userAccountDirectory = new UserAccountDirectory();
+        orgID = counter;
+        ++counter;
+    }
+    
+    public abstract ArrayList<Role> getSupportedRole();
     
 }
