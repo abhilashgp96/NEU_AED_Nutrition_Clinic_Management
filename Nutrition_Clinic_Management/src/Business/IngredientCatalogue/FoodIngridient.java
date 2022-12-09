9aa06035c3a0defe738044521ecd33285aa3a6e9
rayappanor.s@northeastern.edu <rayappanor.s@northeastern.edu>
2022-12-09 02:02:37 -0500

    Updating Food Ingridient class

--- Nutrition_Clinic_Management/src/Business/IngredientCatalogue/FoodIngridient.java
+++ Nutrition_Clinic_Management/src/Business/IngredientCatalogue/FoodIngridient.java
@@ -4,10 +4,27 @@
  */
 package Business.IngredientCatalogue;
 
+import java.util.Date;
+
 /**
  *
  * @author soumya
  */
 public class FoodIngridient {
+    
+    private String foogIngridientName;
+    private int reorderLevel;
+    private int minStock;
+    private int maxStock;
+    private Date expirationDate;
+    private String CateringCategory;
+    private int availableQuantity;
+    private double retailPurchasePrice;
+    private double marketSellingPrice;
+    private int productId;
+    private int serialNumber;
+    private int reqdQuantity;
+    private String reorderStatus="N";
+    
     
 }
